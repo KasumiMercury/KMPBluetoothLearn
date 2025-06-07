6c13f7ea3d0665351da7c3203b3c132ba8991f5b
KasumiMercury <naoki_shira@outlook.jp>
2025-06-07 23:58:16 +0900

    fix

--- composeApp/src/commonMain/kotlin/net/mercuryksm/Platform.kt
+++ composeApp/src/commonMain/kotlin/net/mercuryksm/Platform.kt
@@ -11,25 +11,19 @@ expect fun getPlatform(): Platform
 interface BluetoothProvider {
     fun isBluetoothAvailable(): Boolean
     fun getDeviceList(): List<Device>
-interface BluetoothProvider {
-    fun isBluetoothAvailable(): Boolean
-    fun getDeviceList(): List<Device>
 
     /**
      * Attempts to connect to the specified device.
      * @param device The device to connect to
-     * @return true if connection initiated successfully, false otherwise
      * @throws IllegalArgumentException if device is invalid
      * @throws SecurityException if permissions are insufficient
      */
-    fun connect(device: Device): Boolean
+    fun connect(device: Device)
 
     /**
      * Disconnects from the currently connected device.
-     * @return true if disconnection initiated successfully, false otherwise
      */
-    fun disconnect(): Boolean
-}
+    fun disconnect()
 }
 
 expect fun getBluetoothProvider(): BluetoothProvider
